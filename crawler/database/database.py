6b7c4b577c6e931797020a74c274ae82c5b1998b
baejae <37645490+baejaeho18@users.noreply.github.com>
2024-11-08 07:17:52 +0900

    Update modules: Database

--- crawler/database/database.py
+++ crawler/database/database.py
@@ -16,7 +16,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import logging
-from pymongo.connection import Connection
+# from pymongo.connection import Connection
+from pymongo import MongoClient
 import pymongo
 from models.asyncrequeststructure import AsyncRequestStructure
 from models.timingrequest import TimingRequest
@@ -46,7 +47,8 @@ DOM_ADDRESS = "dom_address"
 class Database():
     
     def __init__(self, db_name, drop_dbs=True):
-        self.connection=Connection()
+        # self.connection=Connection()
+        self.connection=MongoClient()
         self.database = self.connection[db_name]
         self.pages = self.database.pages
         #self.pages.ensure_index( "id", pymongo.ASCENDING, unique=True)
